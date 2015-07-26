69658c1b601420ef53ac6d24f9695dec5c1151aa
Andrii Hrom <groshkka@gmail.com>
2015-07-26 20:38:41 +0300

    develop google map fragment

--- app/src/main/java/com/hrom/andrew/travelshops/Fragments/MapsFragment.java
+++ app/src/main/java/com/hrom/andrew/travelshops/Fragments/MapsFragment.java
@@ -1,6 +1,42 @@
 package com.hrom.andrew.travelshops.Fragments;
 
-import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MapsFragment extends FragmentActivity{
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.hrom.andrew.travelshops.R;
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_maps,container,false);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+    }
 }
