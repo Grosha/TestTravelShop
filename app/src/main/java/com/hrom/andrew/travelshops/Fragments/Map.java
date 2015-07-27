a9237844108e6270cb9bdff8e431c0724c6616f1
Andrii Hrom <groshkka@gmail.com>
2015-07-27 21:01:52 +0300

    add map activity

---
+++ app/src/main/java/com/hrom/andrew/travelshops/Fragments/Map.java
@@ -0,0 +1,56 @@
+package com.hrom.andrew.travelshops.Fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.hrom.andrew.travelshops.R;
+
+/**
+ * Created by Andrew on 26.07.2015.
+ */
+public class Map extends Activity {
+
+    // Google Map
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_maps);
+
+        try {
+            // Loading map
+            initilizeMap();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+}
