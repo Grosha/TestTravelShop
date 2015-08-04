33bfbe0438f8f63c9d7743db8c710540ccd442ff
Andrii Hrom <groshkka@gmail.com>
2015-08-04 23:42:52 +0300

    rename class

---
+++ app/src/main/java/com/hrom/andrew/travelshops/DBCoordinates/CityCoordinate.java
@@ -0,0 +1,7 @@
+package com.hrom.andrew.travelshops.DBCoordinates;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class CityCoordinate {
+    static final LatLng KYEV = new LatLng(50.447968, 30.507971);
+}
