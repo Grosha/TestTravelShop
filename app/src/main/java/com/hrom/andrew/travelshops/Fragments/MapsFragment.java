94e4de764dabb8ab676cc2a5f09c16e87e9188f5
Andrii Hrom <groshkka@gmail.com>
2015-08-04 23:32:59 +0300

    develop MapFragment - now show my location

--- app/src/main/java/com/hrom/andrew/travelshops/Fragments/MapsFragment.java
+++ app/src/main/java/com/hrom/andrew/travelshops/Fragments/MapsFragment.java
@@ -3,6 +3,7 @@ package com.hrom.andrew.travelshops.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,34 +12,46 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.hrom.andrew.travelshops.R;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback {
+public class MapsFragment extends Fragment{
     private GoogleMap googleMap;
+    private SupportMapFragment mapFragment;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.layout_maps, container, false);
-
-        /*MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);*/
-        /*googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-        onMapReady(googleMap);*/
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        mapFragment = SupportMapFragment.newInstance();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.map,mapFragment).commit();
 
-        map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        LatLng KYEV = new LatLng(50.447968, 30.507971);
+
+        if (googleMap == null) {
+            googleMap = mapFragment.getMap();
+            googleMap.addMarker(new MarkerOptions()
+                    .title("Sydney")
+                    .snippet("The most populous city in Australia.")
+                    .position(KYEV));
+        }
+
+        googleMap.setMyLocationEnabled(true);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KYEV, 13));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
     }
 }
