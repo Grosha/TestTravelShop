dfe1c1c0afe388672daf1a0165d3cb97871eb9d6
Andrii Hrom <groshkka@gmail.com>
2015-08-06 22:11:14 +0300

    develop logic for multimap collection

--- app/src/main/java/com/hrom/andrew/travelshops/DBCoordinates/CityCoordinate.java
+++ app/src/main/java/com/hrom/andrew/travelshops/DBCoordinates/CityCoordinate.java
@@ -3,5 +3,5 @@ package com.hrom.andrew.travelshops.DBCoordinates;
 import com.google.android.gms.maps.model.LatLng;
 
 public class CityCoordinate {
-    static final LatLng KYEV = new LatLng(50.447968, 30.507971);
+    public static final LatLng KYEV = new LatLng(50.447968, 30.507971);
 }
