5e35b942b20baeb368864559f3178ad510325419
Andrii Hrom <groshkka@gmail.com>
2015-08-06 22:23:51 +0300

    develop logic for multimap collection

--- app/src/main/java/com/hrom/andrew/travelshops/Fragments/MapActivity.java
+++ app/src/main/java/com/hrom/andrew/travelshops/Fragments/MapActivity.java
@@ -3,7 +3,6 @@ package com.hrom.andrew.travelshops.Fragments;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,14 +11,16 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.hrom.andrew.travelshops.DBCoordinates.CityCoordinate;
 import com.hrom.andrew.travelshops.R;
 import com.hrom.andrew.travelshops.ShopDB.BikeShop;
-import com.hrom.andrew.travelshops.ShopDB.CoordinateShop;
+import com.hrom.andrew.travelshops.DBCoordinates.ShopCoordinate;
+
+import java.util.Collection;
 
 public class MapActivity extends Activity {
     private static final String NAMEFRAGMENT = "FRAGMENT";
-    static final LatLng KYEV = new LatLng(50.447968, 30.507971);
-    private CoordinateShop shop = new CoordinateShop();
+    private ShopCoordinate shop = new ShopCoordinate();
     private BikeShop bike = new BikeShop();
 
     @Override
@@ -30,14 +31,25 @@ public class MapActivity extends Activity {
                 .getMap();
 
         if (map != null) {
-            Marker hamburg = map.addMarker(new MarkerOptions().position(KYEV)
+            Marker city = map.addMarker(new MarkerOptions().position(CityCoordinate.KYEV)
                     .title("KYEV"));
             for (int i = 0; i < bike.getListShops().size(); i++) {
+                Collection<LatLng> colecLng = shop.getCoordinate(bike.getListShops().get(i));
                 Log.d(NAMEFRAGMENT, String.valueOf(bike.getListShops().get(i)));
                 Log.d(NAMEFRAGMENT, String.valueOf(shop.getCoordinate(bike.getListShops().get(i))));
-                if (shop.getCoordinate(bike.getListShops().get(i))==null) {
+                if (shop.getCoordinate(bike.getListShops().get(i)) == null) {
                     continue;
                 } else {
+                    for (LatLng latLng : colecLng) {
+                        Marker shopMarker = map.addMarker(
+                                new MarkerOptions()
+                                        .position(latLng)
+                                        .title(bike.getListShops().get(i))
+                                        .snippet("Kiel is cool")
+                                        .icon(BitmapDescriptorFactory
+                                                .fromResource(bike.getIconShops().get(i))));
+                    }
+                    /*
                     Marker kiel = map.addMarker(
                             new MarkerOptions()
                                     .position(shop.getCoordinate(bike.getListShops().get(i)))
@@ -45,11 +57,13 @@ public class MapActivity extends Activity {
                                     .snippet("Kiel is cool")
                                     .icon(BitmapDescriptorFactory
                                             .fromResource(bike.getIconShops().get(i))));
+                */
                 }
             }
         }
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(KYEV, 15));
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(CityCoordinate.KYEV, 15));
         map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
     }
 }
