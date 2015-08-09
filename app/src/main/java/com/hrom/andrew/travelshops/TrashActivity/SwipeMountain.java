8b440f3806290b2399493b9ad8000726387eaa56
Andrii Hrom <groshkka@gmail.com>
2015-08-09 22:40:16 +0300

    add swipe class

---
+++ app/src/main/java/com/hrom/andrew/travelshops/TrashActivity/SwipeMountain.java
@@ -0,0 +1,22 @@
+package com.hrom.andrew.travelshops.TrashActivity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import com.hrom.andrew.travelshops.R;
+
+public class SwipeMountain extends FragmentActivity{
+    private ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_swipe);
+
+        pager = (ViewPager)findViewById(R.id.pager);
+
+        SwipePageAdapter adapter = new SwipePageAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+    }
+}
