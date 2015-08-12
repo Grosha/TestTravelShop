51fe4dd3e22ed6ad2388560dc2fb932d1a675984
Andrii Hrom <groshkka@gmail.com>
2015-08-13 00:47:09 +0300

    add Tags

--- app/src/main/java/com/hrom/andrew/travelshops/TrashActivity/MyTag.java
+++ app/src/main/java/com/hrom/andrew/travelshops/TrashActivity/MyTag.java
@@ -2,4 +2,9 @@ package com.hrom.andrew.travelshops.TrashActivity;
 
 public class MyTag {
     public static final String NAMEFRAGMENT = "FRAGMENT";
+    public static final String TEST = "TAG_NAME_FRAGMENT";
+    public static String TAG_MOUNTAIN = "1";
+    public static String TAG_SKIS = "2";
+    public static String TAG_SNOWBOARD = "3";
+    public static String TAG_BIKE = "4";
 }
