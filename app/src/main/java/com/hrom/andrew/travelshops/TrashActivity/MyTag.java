e3870e9a1a7be9a9f385f52049254e2db0f344a1
Andrii Hrom <groshkka@gmail.com>
2015-08-22 15:30:25 +0300

    add tag for fragment Map

--- app/src/main/java/com/hrom/andrew/travelshops/TrashActivity/MyTag.java
+++ app/src/main/java/com/hrom/andrew/travelshops/TrashActivity/MyTag.java
@@ -3,8 +3,10 @@ package com.hrom.andrew.travelshops.TrashActivity;
 public class MyTag {
     public static final String NAMEFRAGMENT = "FRAGMENT";
     public static final String TEST = "TAG_NAME_FRAGMENT";
+    public static final String PROGRESS = "PROGRESS";
     public static String TAG_MOUNTAIN = "1";
     public static String TAG_SKIS = "2";
     public static String TAG_SNOWBOARD = "3";
     public static String TAG_BIKE = "4";
+    public static int TAG_MAP = 5;
 }
