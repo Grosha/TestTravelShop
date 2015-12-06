9028f8beb9726065d0e96768d9219fd7e2db80bf
Andrii Hrom <groshkka@gmail.com>
2015-12-06 22:13:59 +0200

    create new adapter for list items on fragment

---
+++ app/src/main/java/com/hrom/andrew/travelshops/costumAdapterListItem/ObjectListItem.java
@@ -0,0 +1,27 @@
+package com.hrom.andrew.travelshops.costumAdapterListItem;
+
+import android.widget.CheckBox;
+
+public class ObjectListItem {
+    private int iconShop;
+    private String nameShop;
+    private Boolean favoriteShop;
+
+    public ObjectListItem(int iconShop, String nameShop, Boolean favoriteShop) {
+        this.iconShop = iconShop;
+        this.nameShop = nameShop;
+        this.favoriteShop = favoriteShop;
+    }
+
+    public Boolean getFavoriteShop() {
+        return favoriteShop;
+    }
+
+    public int getIconShop() {
+        return iconShop;
+    }
+
+    public String getNameShop() {
+        return nameShop;
+    }
+}
